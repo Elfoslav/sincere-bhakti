2176685cdc03db05f02f64fbd83ef3cb59711c93
Tomas <tom.hromnik@gmail.com>
2026-07-06 12:41:47 +0200

    Fix tests and deployment

--- src/lib/services/upload.ts
+++ src/lib/services/upload.ts
@@ -1,4 +1,4 @@
-import { randomUUID } from "crypto";
+import { randomUUID } from "node:crypto";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
