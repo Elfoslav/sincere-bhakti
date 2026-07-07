47b0c44431798c7adacab15d2c4ae21ef9612c75
Tomas <tom.hromnik@gmail.com>
2026-07-07 19:13:32 +0200

    Move Sentry DSN from hardcoded to SENTRY_DSN env var

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://cb78b18cf6bf76111a55a6486d882533@o4511292367175680.ingest.de.sentry.io/4511627155341392",
+  dsn: process.env.SENTRY_DSN,
 
   enabled: process.env.NODE_ENV === "production",
 
